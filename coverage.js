dd6a74a2815ebe865c269eb81b93c28c007523ab
l0ckstreet <rsalas7793@gmail.com>
2020-12-06 08:57:16 +0800

    haha

--- coverage.js
+++ coverage.js
@@ -1,6 +1,7 @@
 const computePV = require("./computePV")
 
 const costArray = require("./input")
+const partitionCosts = require("./partitionCosts")
 
 const equityRate = 10
 const proRate = 7
@@ -19,50 +20,19 @@ const equityPeriod = 19
 //year10 - year19 7%    [9-18]
 //year 20 -             [19-length]
 
-const cashArray = []
-const moneyArray = []
-const fourArray = []
-const sevenArray = []
-const tenArray = []
-
-// for (let i = 0; i < costArray.length; i++) {
-// 	const reduce
-
-// }
-
-for (let i = 0; i < costArray.length; i++) {
-	if (i <= 2) {
-		cashArray.push(costArray[i])
-	} else if (i > 2 && i <= 3) {
-		moneyArray.push(costArray[i])
-	} else if (i > 3 && i <= 8) {
-		fourArray.push(costArray[i])
-	} else if (i > 8 && i <= 18) {
-		sevenArray.push(costArray[i])
-	} else {
-		tenArray.push(costArray[i])
-	}
-}
-
-//add zeroes to array
-//cash array [Sum only]
+const {
+	cashArray,
+	moneyArray,
+	fourArray,
+	sevenArray,
+	tenArray
+} = partitionCosts(costArray)
 
 console.log("cash", cashArray)
-
 console.log("money", moneyArray)
 console.log("four", fourArray)
 console.log("seven", sevenArray)
 console.log("10", tenArray)
-
-//if array [] pv = 0
-//if array [1elem] discount
-//if array [2elem] discount sum
-//if array [>2]
-
-//pop first element of array
-//get NPV of remaining
-//add
-//get present value
 
 const cashPV = cashArray[0] + cashArray[1] + cashArray[2]
 const moneyPV = computePV(moneyArray, moneyPeriod, moneyRate)
@@ -75,4 +45,3 @@ console.log(cashPV, moneyPV, bondPV, proPV, equityPV)
 const coverage = cashPV + moneyPV + bondPV + proPV + equityPV
 
 console.log(coverage.toFixed(2))
-//compute for NPVs
--- partitionCosts.js
+++ partitionCosts.js
@@ -6,14 +6,23 @@ module.exports = function partionCosts(costArray) {
 	const tenArray = []
 
 	for (let i = 0; i < costArray.length; i++) {
+		//cash flow first 3 years
 		if (i <= 2) {
 			cashArray.push(costArray[i])
+
+			//cash flow @ 4 years
 		} else if (i > 2 && i <= 3) {
 			moneyArray.push(costArray[i])
+
+			//cash flow 5 years to 9 years
 		} else if (i > 3 && i <= 8) {
 			fourArray.push(costArray[i])
+
+			//cash flow 10 years to 19 years
 		} else if (i > 8 && i <= 18) {
 			sevenArray.push(costArray[i])
+
+			//cash flow 20 years and above
 		} else {
 			tenArray.push(costArray[i])
 		}
